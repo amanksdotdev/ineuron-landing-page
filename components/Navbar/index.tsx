8878995be552ab4e034112f5282af616d498098b
Aman Singh <amanksofficial@gmail.com>
2022-08-04 16:18:04 +0530

    first iteration of mobile navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,12 +3,15 @@ import { VscChevronDown, VscChevronRight } from "react-icons/vsc";
 import Card from "./Card";
 import { COMPANY, COURSES, NAV_LINKS } from "./constants";
 import { NavItem } from "./NavItem";
-import { FiSearch } from "react-icons/fi";
-
+import { FiCrosshair, FiSearch } from "react-icons/fi";
+import { GiClosedBarbute, GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+import { Container } from "../Container";
 export const Navbar = () => {
   const [showCourses, setShowCourses] = useState(false);
   const [showCompanyDropdown, setShowCompanyDropdown] = useState(false);
   const navRef = useRef<HTMLElement>(null);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     const scrollFn = () => {
@@ -73,7 +76,51 @@ export const Navbar = () => {
           <NavItem className="px-4 py-2 hover:bg-[rgba(0,0,0,0.1)] rounded ease-in duration-200">
             <a href="">Login</a>
           </NavItem>
+          <NavItem>
+            <GiHamburgerMenu
+              className="md:hidden"
+              onClick={() => setNavOpen(true)}
+            />
+          </NavItem>
         </div>
+      </div>
+      <div
+        className={`w-screen h-screen bg-[#fff0e5] z-30 absolute inset-0 p-2 ${
+          !navOpen ? "hidden" : ""
+        }`}
+      >
+        <MdClose
+          className="absolute right-5 text-3xl"
+          onClick={() => setNavOpen(false)}
+        />
+        <h1 className="text-lg text-center mb-4">Menu</h1>
+        <Container>
+          <div className="mb-4 text-sm">
+            <div className="my-2 py-3 border-b border-blue-500 flex items-center gap-2">
+              Courses <VscChevronRight />
+            </div>
+            {NAV_LINKS.map((nav) => {
+              return (
+                <div
+                  key={nav.text}
+                  className="my-2 py-3 border-b border-blue-500 flex items-center gap-2"
+                >
+                  {nav.text}
+                </div>
+              );
+            })}
+            <div className="my-2 py-3 border-b border-blue-500 flex items-center gap-2">
+              Company <VscChevronRight />
+            </div>
+          </div>
+
+          <button className="w-full bg-blue-500 text-white font-bold py-4 mb-4">
+            Sign In
+          </button>
+          <button className="w-full border-blue-500 border-[1px]  text-blue-500 font-bold py-4">
+            Sign Up
+          </button>
+        </Container>
       </div>
     </nav>
   );
